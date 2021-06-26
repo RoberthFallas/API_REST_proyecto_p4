e5b23db9191ea923a2c877706a80bb1ea47ed075
GerardoAraya <gera.araya3@gmail.com>
2021-06-25 22:30:03 -0600

    comentario

--- controllers/ctrl_suscripciones.py
+++ controllers/ctrl_suscripciones.py
@@ -2,7 +2,7 @@ from flask import json, jsonify, request, flash, redirect, url_for
 from init import app
 from werkzeug.utils import secure_filename
 from services import srv_suscripciones
-
+#obtiene 
 @app.route('/get_suscripcion/<int:idCliente>/<int:idTienda>')        
 def get_subscripcion(idCliente,idTienda):
         resp=srv_suscripciones.get_suscripcion(idCliente,idTienda)
@@ -14,6 +14,7 @@ def get_subscripcion(idCliente,idTienda):
             content={}
         return jsonify(json_items)
 
+#Agregar suscripciones 
 @app.route('/agregar_suscripcion',methods=['POST']) 
 def agregar_suscripcion():
      _json=request.get_json(force=True)
@@ -21,11 +22,14 @@ def agregar_suscripcion():
      resp = jsonify('suscripcion Guardada.')
      resp.status_code = 200
      return resp
-
+ 
+ #eliminar  las suscripciones mediante id
 @app.route('/eliminar_suscripcion/<int:idComprador>/<int:idTienda>',methods=['DELETE']) 
 def eliminar_suscripcion(idComprador,idTienda):
      resp=srv_suscripciones.eliminar_suscripcion(idComprador,idTienda)
      return resp
+
+ #obtiene las suscripciones de los clientes mediante id
 @app.route('/get_miSuscripcion/<int:idCliente>')        
 def get_miSuscripcion(idCliente):
         resp=srv_suscripciones.get_miSuscripcion(idCliente)
